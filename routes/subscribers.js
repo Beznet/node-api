55fad5fb3c22b581b26711f96d14ea43fe2f0aae
Bennett Dungan <bennettdungan@gmail.com>
2019-07-18 22:21:25 -0500

    added Update Sub and Delete Sub routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -11,10 +11,12 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 })
+
 // Getting one subscriber
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
 })
+
 // Creating one subscriber
 router.post('/', async (req, res) => {
   const subscriber = new Subscriber({
@@ -29,13 +31,32 @@ router.post('/', async (req, res) => {
     res.status(400).json({ message: err.message })
   }
 })
+
 // Updating one subscriber
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch {
+    res.status(400).json({ message: err.message })
+  }
 
 })
-// Deleteing one subscriber
-router.delete('/:id', (req, res) => {
-
+// Deleting one subscriber
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted This Subscriber' })
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 async function getSubscriber(req, res, next) {
